e72edf3c1ad5a7a50dd07a6b5ceba534ba6d1ef4
Mauro Espinal V <mauro@MacBook-Pro.local>
2020-04-25 22:25:29 -0500

    con la . carpeta build

---
+++ proyecto-citas/build/precache-manifest.3515508c1053d49be24c67faa53debc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf67178d24cdf2426790ee59b66e0b1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e45edf06ccc5a3cc04ed",
+    "url": "/static/css/main.c9ea984c.chunk.css"
+  },
+  {
+    "revision": "03878e1334ee48a66a9a",
+    "url": "/static/js/2.23806998.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.23806998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e45edf06ccc5a3cc04ed",
+    "url": "/static/js/main.9361c04d.chunk.js"
+  },
+  {
+    "revision": "d14b8d3b66d7b3cca564",
+    "url": "/static/js/runtime-main.fdcac9ed.js"
+  }
+]);
